c6c70a57ed5109bbe63dd66435c2e028a1084f5d
rohitu0028 <rohitu4428@gmail.com>
2025-06-26 20:04:02 +0530

    assignment5 Print a solid square pattern

---
+++ squarePattern.c
@@ -0,0 +1,14 @@
+void main() 
+{
+    int n, i, j;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
