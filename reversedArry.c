751d91f17e5f787ea69e87ad076c16ee2a7d583f
rohitu0028 <rohitu4428@gmail.com>
2025-06-26 21:06:42 +0530

    assignment7 Reverse the given array.

---
+++ reversedArry.c
@@ -0,0 +1,17 @@
+void main() 
+{
+    int arr[100], n, i;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Reversed array:\n");
+    for(i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+}
