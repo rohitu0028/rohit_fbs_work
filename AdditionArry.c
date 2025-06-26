58688e3dd2a4f4742aa299051c0021101f72198a
rohitu0028 <rohitu4428@gmail.com>
2025-06-26 21:05:26 +0530

    assignment7 Take two array and add sum in third array

---
+++ AdditionArry.c
@@ -0,0 +1,25 @@
+void main() 
+{
+    int arr[5], brr[5], crr[5];
+    int i;
+
+    printf("Enter 5 elements for first array:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter 5 elements for second array:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &brr[i]);
+    }
+
+    for(i = 0; i < 5; i++) {
+        crr[i] = arr[i] + brr[i];
+    }
+
+    printf("Resulting array after addition:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", crr[i]);
+    }
+
+}
